4863453ba96cb27deee05513d07cb165b9c5c280
Angel <angel.cabrera@lc.cuny.edu>
2024-12-21 21:28:45 -0500

    populated img2 page+added script

---
+++ scriptimg2.js
@@ -0,0 +1,32 @@
+var submitAnswer = function() {
+
+    var radios = document.getElementsByName('choice');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please select one');
+    }
+    if (val == "Yes") {
+        alert('I thought it would be funny to leave this page blank to fit in with the question, My bad');
+    }
+    if (val == "No") {
+        alert('Great!');
+    }
+    if (val == "Probably") {
+        alert('Lets hope not! This was intentionally not styled to fit with the question');
+    }
+    //} else if ( val == "Yes" ) {
+     // alert('I thought it would be funny to leave this page blank to fit in with the question, My bad');
+    //} else if( val == "No" ) {
+     // alert('Great!');
+    //} else {
+     //   alert('Lets hope not! This was intentionally not styled to fit with the question');
+    //}
+
+  };
